fceac6fe4b4126f6fbd7d6658cab2e1a07ad54f5
madgrid <aaronmadved@gmail.com>
2019-08-04 15:57:30 +0800

    added default route to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { SenderAddressComponent } from './components/sender-address/sender-address.component';
+// import { ReceiverAddressComponent } from './components/receiver-address/receiver-address.component';
+import { WizardComponent } from './components/wizard/wizard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    // path: 'wizard', children: [
+    //   { path: 'sender', component: SenderAddressComponent },
+    //   { path: 'receiver', component: ReceiverAddressComponent },
+    // ]
+    path: 'wizard', component: WizardComponent
+  },
+  {
+    path: '',
+    redirectTo: 'wizard',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
