2126ea7b19097f709c6d87e560d89e0dddf99f14
madgrid <aaronmadved@gmail.com>
2019-08-04 15:59:14 +0800

    removed comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// import { SenderAddressComponent } from './components/sender-address/sender-address.component';
-// import { ReceiverAddressComponent } from './components/receiver-address/receiver-address.component';
 import { WizardComponent } from './components/wizard/wizard.component';
 
 
 const routes: Routes = [
   {
-    // path: 'wizard', children: [
-    //   { path: 'sender', component: SenderAddressComponent },
-    //   { path: 'receiver', component: ReceiverAddressComponent },
-    // ]
     path: 'wizard', component: WizardComponent
   },
   {
