2a1164f35b6c62bba720e16c4db2151b9c1cc802
madgrid <aaronmadved@gmail.com>
2019-08-04 15:57:07 +0800

    added flex, material, and basic shipping components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SenderAddressComponent } from './components/sender-address/sender-address.component';
+import { ReceiverAddressComponent } from './components/receiver-address/receiver-address.component';
+
+import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatProgressBarModule, MatCardModule, MatTabsModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WizardComponent } from './components/wizard/wizard.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SenderAddressComponent,
+    ReceiverAddressComponent,
+    WizardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
