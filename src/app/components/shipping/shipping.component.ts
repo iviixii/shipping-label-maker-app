0d3836f91cf5701f181fddc6df1244aceaa0165d
madgrid <aaronmadved@gmail.com>
2019-08-05 18:30:46 +0800

    added form, eventemitter, radio, flex, queryParams to shipping

---
+++ src/app/components/shipping/shipping.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.scss']
+})
+export class ShippingComponent implements OnInit {
+  shippingForm: FormGroup;
+  @Output() shippingOutput = new EventEmitter<number>();
+
+  constructor(fb: FormBuilder, private router: Router) {
+    this.shippingForm = fb.group({
+      shippingOption: new FormControl('', [
+        Validators.required
+       ])
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onFormSubmit(direction: string) {
+    console.log(this.shippingForm.value);
+    this.shippingOutput.emit(this.shippingForm.value);
+    if (direction === 'next') {
+      this.router.navigate(['wizard'],
+        { queryParams: { index: 4, progress: 100 } });
+    } else {
+      this.router.navigate(['wizard'],
+        { queryParams: { index: 2, progress: 60 } });
+    }
+  }
+}
