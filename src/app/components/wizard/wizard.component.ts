12abca6440827aa97b7a7ab164905a2d1e3c2aa3
madgrid <aaronmadved@gmail.com>
2019-08-05 18:35:31 +0800

    added handle data store in interface

--- src/app/components/wizard/wizard.component.ts
+++ src/app/components/wizard/wizard.component.ts
@@ -1,4 +1,6 @@
+import { IShippingLabel } from './../../models/shipping.model';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-wizard',
@@ -10,16 +12,47 @@ export class WizardComponent implements OnInit {
   progress: string;
   wizardIndex: number;
 
-  constructor() {
+  shippingInfo: IShippingLabel;
+
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.shippingInfo = {};
+
+    this.activatedRoute.queryParams.subscribe(params => {
+      if (params['progress']) {
+        this.progress = params['progress']
+      } else {
+        // reset both bar and tabs
+        this.progress = '20';
+        this.wizardIndex = 0;
+      }
+      if (params['index']) {
+        this.wizardIndex = params['index']
+      } else {
+        // reset both bar and tabs
+        this.progress = '20';
+        this.wizardIndex = 0;
+      }
+    });
   }
 
   ngOnInit() {
-    this.barValue = '20';
-    this.wizardIndex = 0;
   }
 
-  changeWizardIndex(changingValue: number) {
-    this.wizardIndex = this.wizardIndex + changingValue;
+  updateReceiverInfo($event) {
+    this.shippingInfo.to = $event;
+    console.log(this.shippingInfo);
+  }
+  updateSenderInfo($event) {
+    this.shippingInfo.from = $event;
+    console.log(this.shippingInfo);
+  }
+  updateWeightInfo($event) {
+    this.shippingInfo.weight = $event.weight;
+    console.log(this.shippingInfo);
+  }
+  updateShippingInfo($event) {
+    this.shippingInfo.shippingOption = $event.shippingOption;
+    console.log(this.shippingInfo);
   }
 
 }
