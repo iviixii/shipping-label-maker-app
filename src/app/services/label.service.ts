5bed349670eddca726139bdfff7c82abf808f012
madgrid <aaronmadved@gmail.com>
2019-08-06 14:36:19 +0800

    created service for label creation

---
+++ src/app/services/label.service.ts
@@ -0,0 +1,17 @@
+import { IShippingLabel } from './../models/shipping.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LabelService {
+
+  constructor(private http: HttpClient) { }
+
+  create(labelInfo?: IShippingLabel) {
+    // example API call
+    return this.http.post<any>(`${environment.server_url}/label/create`, { labelInfo });
+  }
+}
